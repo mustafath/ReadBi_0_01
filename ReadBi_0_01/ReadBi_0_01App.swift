8ee00ef5e5b55b8032932a25a31ea020c4cf990e
mustafath <mustafadalkesen11@gmail.com>
2023-03-26 15:56:27 +0300

    Initial Commit

---
+++ ReadBi_0_01/ReadBi_0_01App.swift
@@ -0,0 +1,17 @@
+//
+//  ReadBi_0_01App.swift
+//  ReadBi_0_01
+//
+//  Created by Mustafa Girgin on 26.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReadBi_0_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
